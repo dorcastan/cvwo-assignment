2a4c29ba2dbc469b773550bbaa3b0d1b0dc21e4d
dorcastan <dorcas.tann@gmail.com>
2019-12-28 17:28:46 +0800

    Add: To-Do deletion
    Add handling for different HTTP response codes.
    
    Fix code style and potential errors using ESLint and Prettier (Now).

--- app/javascript/components/TodoList.js
+++ app/javascript/components/TodoList.js
@@ -1,10 +1,10 @@
-import React from "react";
 import { Link } from "@reach/router";
+import React from "react";
 import ShowTodos from "./ShowTodos";
 
 const TodoList = () => (
     <div>
-        <h1>Your To-Dos</h1>
+        <h1>To-Dos</h1>
         <ShowTodos />
         <Link to="/add">Add a new to-do</Link>
     </div>
