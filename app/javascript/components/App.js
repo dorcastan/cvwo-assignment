87777889ba6da900c1bb2e97a3c2cda503a0cf1f
dorcastan <dorcas.tann@gmail.com>
2020-01-09 16:05:44 +0800

    Fix: Missing route to search component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,7 @@
 import { Router } from '@reach/router';
 import React from 'react';
 import AddTodo from './AddTodo';
+import SearchTodos from './SearchTodos';
 import TodoList from './TodoList';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
         <Router>
             <TodoList path='/' />
             <AddTodo path='/add' />
+            <SearchTodos path='/search' />
         </Router>
     );
 }
