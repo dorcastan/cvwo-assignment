fa512e3d00b93eb8cbc72dafd30efdb17adeb45c
dorcastan <dorcas.tann@gmail.com>
2020-01-18 10:17:37 +0800

    Fix: Elevation of title and header

--- app/javascript/components/AppHeader.js
+++ app/javascript/components/AppHeader.js
@@ -1,4 +1,4 @@
-import { AppBar, Box, Button, Grid, Typography } from '@material-ui/core';
+import { AppBar, Box, Button, Grid, Paper, Typography } from '@material-ui/core';
 import { Add, Search } from '@material-ui/icons';
 import { Link as RouterLink } from '@reach/router';
 import React from 'react';
@@ -6,37 +6,45 @@ import HomeButton from './HomeButton';
 
 const AppHeader = () => (
     <Box mb={3} border={1} borderRadius='borderRadius' borderColor='primary.main'>
-        <AppBar position='static'>
-            <Box align='center' padding={2}>
-                <Typography variant='h2' component='h1'>
-                    To-Do List
-                </Typography>
-            </Box>
-        </AppBar>
+        <Paper elevation={3}>
+            <AppBar position='static' elevation={0}>
+                <Box align='center' padding={2}>
+                    <Typography variant='h2' component='h1'>
+                        To-Do List
+                    </Typography>
+                </Box>
+            </AppBar>
 
-        <Box padding={2}>
-            <Grid container spacing={2}>
-                <Grid item>
-                    <HomeButton />
+            <Box padding={2}>
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <HomeButton />
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            component={RouterLink}
+                            to='/add'
+                            variant='contained'
+                            color='primary'
+                            startIcon={<Add />}
+                        >
+                            Add to-do
+                        </Button>
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            component={RouterLink}
+                            to='/search'
+                            variant='contained'
+                            color='secondary'
+                            startIcon={<Search />}
+                        >
+                            Search to-dos
+                        </Button>
+                    </Grid>
                 </Grid>
-                <Grid item>
-                    <Button component={RouterLink} to='/add' variant='contained' color='primary' startIcon={<Add />}>
-                        Add a to-do
-                    </Button>
-                </Grid>
-                <Grid item>
-                    <Button
-                        component={RouterLink}
-                        to='/search'
-                        variant='contained'
-                        color='secondary'
-                        startIcon={<Search />}
-                    >
-                        Search to-dos
-                    </Button>
-                </Grid>
-            </Grid>
-        </Box>
+            </Box>
+        </Paper>
     </Box>
 );
 
