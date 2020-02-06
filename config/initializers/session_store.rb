1c302d4cb5431c1346ecfeb1fd0e803c70c3b557
dorcastan <dorcas.tann@gmail.com>
2020-02-06 15:51:42 +0800

    Fix: Disable domain in production session store
    So that session cookies will be set. Unable to fix the domain name such
    that users stay logged in (required for to-do creation).

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
-if Rails.env === 'production'
-    Rails.application.config.session_store :cookie_store, key: '_todolist_app', domain: 'http://todolist-9920.herokuapp.com/'
-else
-    Rails.application.config.session_store :cookie_store, key: '_todolist_app'
-end
+# if Rails.env === 'production'
+#     Rails.application.config.session_store :cookie_store, key: '_todolist_app', domain: 'http://todolist-9920.herokuapp.com/'
+# else
+Rails.application.config.session_store :cookie_store, key: '_todolist_app'
+# end
